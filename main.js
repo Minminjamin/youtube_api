1fa7247600bc285df9289f05e5f271d0259fcc98
Minminjamin <0429el@gmail.com>
2023-09-22 15:41:58 +0900

    style

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const frame = document.querySelector("section");
+const pic = document.querySelectorAll(".pic");
 const YOUTUBE_API_KEY = "AIzaSyAWxGBPSbD77v0zph1g-avF18Tv5Cqt3Mk";
 const BASE_URL = " https://www.googleapis.com/youtube/v3/playlistItems";
 const pid = "PLNXichiUWg4AMRZ2o73mjBIvA2f5Pi6-4";
@@ -7,6 +8,17 @@ const num = 5;
 const tit_len = 50;
 const desc_len = 180;
 const resultURL = `${BASE_URL}?key=${YOUTUBE_API_KEY}&part=snippet&playlistId=${pid}&maxResults=${5}`;
+
+// 이벤트 위임(event delegation)
+// 동적으로  생성되는 요소에 이벤트 연결이 불가, 이벤트 연결시점에 해당 돔이 생성되지 않았기 때문
+// 항상 있는 body 요소에다가 이벤트를 위임해서 추후 동적 dom이 생기면 이벤트를 전달받도록 처리
+window.addEventListener("click", (e) => {
+  //e.currentTarget : 이벤트가 연결되어 있는 선택자를 반환
+  //e.target : 실제화면상에서 이벤트가 발생한 요소를 반환
+  if (e.target.nodeName === "IMG") {
+    console.log("You clicked Pic");
+  }
+});
 
 fetch(resultURL)
   .then((data) => data.json())
