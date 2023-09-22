ff6a0dfd71faca74457b0817c0c2d7ae3e026fb1
Minminjamin <0429el@gmail.com>
2023-09-22 14:14:42 +0900

    style : 본문 내용 자르기

--- main.js
+++ main.js
@@ -3,23 +3,31 @@ const frame = document.querySelector("section");
 const YOUTUBE_API_KEY = "AIzaSyAWxGBPSbD77v0zph1g-avF18Tv5Cqt3Mk";
 const BASE_URL = " https://www.googleapis.com/youtube/v3/playlistItems";
 const pid = "PLNXichiUWg4AMRZ2o73mjBIvA2f5Pi6-4";
+
 const num = 5;
+const tit_let = 50;
 
 const resultURL = `${BASE_URL}?key=${YOUTUBE_API_KEY}&part=snippet&playlistId=${pid}&maxResults=${5}`;
 
 fetch(resultURL)
   .then((data) => data.json())
   .then((json) => {
-    console.log(json.items);
-
     let tags = "";
 
     json.items.map((data) => {
+      let desc = data.snippet.description;
+
+      desc.length > 120 ? (desc = desc.substr(0, 120) + "...") : desc;
+
       tags += `
       <article>
-        <h2>${data.snippet.title}</h2>
+        <h2>${
+          data.snippet.title.length > tit_let
+            ? data.snippet.title.substr(0, tit_let) + "..."
+            : data.snippet.title
+        }</h2>
         <div class="text">
-          <p>${data.snippet.description}</p>
+          <p>${desc}</p>
           <span>${data.snippet.publishedAt}</span>
         </div>
         <div class="pic">
