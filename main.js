954aa33d0d13d5ba7e8ddff353ada72cba24664a
Minminjamin <0429el@gmail.com>
2023-09-22 12:47:00 +0900

    feature ; 유튜브 api 연결

---
+++ main.js
@@ -0,0 +1,13 @@
+const YOUTUBE_API_KEY = "AIzaSyAWxGBPSbD77v0zph1g-avF18Tv5Cqt3Mk";
+const BASE_URL = " https://www.googleapis.com/youtube/v3/playlistItems";
+const pid = "PLNXichiUWg4AMRZ2o73mjBIvA2f5Pi6-4";
+const num = 5;
+
+const resultURL = `${BASE_URL}?key=${YOUTUBE_API_KEY}&part=snippet&playlistId=${pid}&maxResults=${5}`;
+
+fetch(resultURL)
+  .then((data) => data.json())
+  .then((json) => {
+    console.log(json);
+  });
+// console.log(resultURL);
