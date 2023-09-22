40ba26e640d367a94af0c5050c5b59e693b832ab
Minminjamin <0429el@gmail.com>
2023-09-22 15:23:04 +0900

    refact

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 const frame = document.querySelector("section");
-
 const YOUTUBE_API_KEY = "AIzaSyAWxGBPSbD77v0zph1g-avF18Tv5Cqt3Mk";
 const BASE_URL = " https://www.googleapis.com/youtube/v3/playlistItems";
 const pid = "PLNXichiUWg4AMRZ2o73mjBIvA2f5Pi6-4";
 
 const num = 5;
-const tit_let = 50;
-
+const tit_len = 50;
+const desc_len = 180;
 const resultURL = `${BASE_URL}?key=${YOUTUBE_API_KEY}&part=snippet&playlistId=${pid}&maxResults=${5}`;
 
 fetch(resultURL)
@@ -14,35 +13,30 @@ fetch(resultURL)
   .then((json) => {
     let tags = "";
 
-    let text = "beef-lettuce-tomato";
-    text = text
-      .split("-") //배열 분리의 기준값
-      .map((el) => el.charAt(0).toUpperCase() + el.slice(1)) //분리된 문자값을 반복될면서 첫번째 글자만 대문자 변경 + 나머지 문자 이어붙이기
-      .join(" "); //각 단어들을 빈칸으로 합치기
-
-    console.log(text);
     json.items.map((data) => {
       let desc = data.snippet.description;
-      let date = data.snippet.publishedAt.split("T")[0];
+      desc.length > desc_len ? (desc = desc.substr(0, desc_len) + "...") : desc;
 
+      //날자값 가공
+      let date = data.snippet.publishedAt.split("T")[0];
       date = date.split("-").join(".");
-      desc.length > 120 ? (desc = desc.substr(0, 120) + "...") : desc;
+
       tags += `
-      <article>
-        <h2>${
-          data.snippet.title.length > tit_let
-            ? data.snippet.title.substr(0, tit_let) + "..."
-            : data.snippet.title
-        }</h2>
-        <div class="text">
-          <p>${desc}</p>
-          <span>${date}</span>
-        </div>
-        <div class="pic">
-          <img src='${data.snippet.thumbnails.standard.url}'/>
-        </pic>
-      </article>
-      `;
+				<article>
+					<h2>${
+            data.snippet.title.length > tit_len
+              ? data.snippet.title.substr(0, tit_len) + "..."
+              : data.snippet.title
+          }</h2>
+					<div class='txt'>
+						<p>${desc}</p>
+						<span>${date}</span>
+					</div>
+					<div class='pic'>
+						<img src='${data.snippet.thumbnails.standard.url}' />
+					</div>					
+				</article>
+			`;
     });
 
     frame.innerHTML = tags;
